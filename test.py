ce32de1b793c40c1af35d61d03f71687df903315
hannahmaung <hannahmaung33@gmail.com>
2021-06-06 17:18:14 -0700

    created test and example

---
+++ test.py
@@ -0,0 +1,20 @@
+import unittest 
+import example
+
+
+class TestCase(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(example.add(1,2),3)
+        
+    def test_sub(self):
+        self.assertEqual(example.subtract(10,5),5)    
+
+    def test_mult(self):
+        self.assertEqual(example.multiply(1,2),2)
+
+    def test_div(self):
+        self.assertEqual(example.divide(10,2),5)
+
+if __name__ == '__main__':
+    unittest.main()
