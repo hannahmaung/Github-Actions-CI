23ccd6535170bb9179f15351ea9418f2af1791d6
hannahmaung <hannahmaung33@gmail.com>
2021-06-06 17:21:13 -0700

    Update test.py

--- test.py
+++ test.py
@@ -5,16 +5,18 @@ import example
 class TestCase(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(example.add(1,2),3)
+        self.assertEqual(example.add(1, 2), 3)
         
     def test_sub(self):
-        self.assertEqual(example.subtract(10,5),5)    
+        self.assertEqual(example.subtract(10, 5), 5)    
 
     def test_mult(self):
-        self.assertEqual(example.multiply(1,2),2)
+        self.assertEqual(example.multiply(1, 2), 2)
 
     def test_div(self):
-        self.assertEqual(example.divide(10,2),5)
+        self.assertEqual(example.divide(10, 2), 5)
 
 if __name__ == '__main__':
     unittest.main()
+
+    
